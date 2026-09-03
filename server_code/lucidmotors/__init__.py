6016311131c583194007381ce67bf2bdbb5827ae
jonathan.falk@marginalutilityllc.com <jonathan.falk@marginalutilityllc.com>
2026-09-03 15:40:00 +0000

    Edited Form1, Layouts, Layouts.BaseLayout, ServerModule1 and 19 more

--- server_code/lucidmotors/__init__.py
+++ server_code/lucidmotors/__init__.py
@@ -1,3 +1,6 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 """The Lucid Motors mobile app API"""
 
 from __future__ import annotations
--- server_code/lucidmotors/exceptions.py
+++ server_code/lucidmotors/exceptions.py
@@ -1,3 +1,6 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 """Exception types for the Lucid Motors API."""
 
 from dataclasses import dataclass
